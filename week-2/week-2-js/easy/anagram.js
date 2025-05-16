a942a453a5f2bd4e79aecd4769db5607764515d2
Shubham Gupta <114250610+shub7800@users.noreply.github.com>
2025-05-16 15:12:36 +0530

    Update anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
+    // Convert both strings to lowercase
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
 
+    // Split, sort, and join the strings
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
 
+// Export the function for use in other files
 module.exports = isAnagram;
+
